5449d26b8c667e8f904ec157422899009f72b89e
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-17 13:50:17 +0800

    [Felicity] refactor: extract method in PigLatin

--- src/main/java/com/tw/academy/basic/$4_naming/practiceTwo/PigLatin.java
+++ src/main/java/com/tw/academy/basic/$4_naming/practiceTwo/PigLatin.java
@@ -1,26 +1,29 @@
 package com.tw.academy.basic.$4_naming.practiceTwo;
 
 public class PigLatin {
-    //Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
     public static String pigIt(String str) {
         String[] punctuation={".",",","-",":",";","!","?"};
         String[] word = str.split(" ");
-        char c;
-        boolean keepOriginChar = true;
-
         for (int i = 0; i < word.length; i++){
-            for (int j = 0; j < punctuation.length; j++)
-                if(word[i].contains(punctuation[j]) == true)
-                    keepOriginChar = false;
-
-            if (keepOriginChar){
-                c = word[i].charAt(0);
-                word[i] = word[i].substring(1, word[i].length());
-                word[i] = word[i].replace(word[i], word[i] + c + "ay");
+            if (isCurrentWordContainsPunctuation(punctuation, word[i])) {
+                moveCurrentWordFirstCharToEndAddAy(word, i);
             }
-            keepOriginChar = true;
         }
-
         return String.join(" ", word);
+    }
+
+    private static void moveCurrentWordFirstCharToEndAddAy(String[] word, int i) {
+        char c = word[i].charAt(0);
+        word[i] = word[i].substring(1);
+        word[i] = word[i].replace(word[i], word[i] + c + "ay");
+    }
+
+    private static boolean isCurrentWordContainsPunctuation(String[] punctuation, String s) {
+        for (int j = 0; j < punctuation.length; j++) {
+            if(s.contains(punctuation[j])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
