055e813c15677d7152728dc67c547073be85beab
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-17 15:28:24 +0800

    [Felicity] refactor: ranaming in PigLatin

--- src/main/java/com/tw/academy/basic/$4_naming/practiceTwo/PigLatin.java
+++ src/main/java/com/tw/academy/basic/$4_naming/practiceTwo/PigLatin.java
@@ -3,22 +3,23 @@ package com.tw.academy.basic.$4_naming.practiceTwo;
 public class PigLatin {
 
     public static final String WORD_SEPARATOR = " ";
+    public static final String DECODED_SUFFIX = "ay";
 
-    public static String pigIt(String str) {
+    public static String pigIt(String sentence) {
         String[] punctuation={".",",","-",":",";","!","?"};
-        String[] word = str.split(WORD_SEPARATOR);
-        for (int i = 0; i < word.length; i++){
-            if (isCurrentWordContainsPunctuation(punctuation, word[i])) {
-                moveCurrentWordFirstCharToEndAddAy(word, i);
+        String[] words = sentence.split(WORD_SEPARATOR);
+        for (int i = 0; i < words.length; i++){
+            if (isCurrentWordContainsPunctuation(punctuation, words[i])) {
+                moveCurrentWordFirstCharToEndAddAy(words, i);
             }
         }
-        return String.join(WORD_SEPARATOR, word);
+        return String.join(WORD_SEPARATOR, words);
     }
 
     private static void moveCurrentWordFirstCharToEndAddAy(String[] word, int i) {
-        char c = word[i].charAt(0);
+        char firstLetter = word[i].charAt(0);
         word[i] = word[i].substring(1);
-        word[i] = word[i].replace(word[i], word[i] + c + "ay");
+        word[i] = word[i].replace(word[i], word[i] + firstLetter + DECODED_SUFFIX);
     }
 
     private static boolean isCurrentWordContainsPunctuation(String[] punctuation, String s) {
