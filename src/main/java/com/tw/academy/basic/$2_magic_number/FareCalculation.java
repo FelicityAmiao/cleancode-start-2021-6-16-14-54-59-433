8582d6df73a03fd0a021f755516bfe3ed7e73ad6
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-17 12:35:32 +0800

    [Felicity] refactor: eliminate magic number

--- src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
+++ src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
@@ -1,13 +1,18 @@
 package com.tw.academy.basic.$2_magic_number;
 
 public class FareCalculation {
+    public final static int ORDINARY_EXPENSES_MAX = 150;
+    public final static int ORDINARY_EXPENSES_MIN = 100;
+    public final static int SPECIAL_EXPENSES_MAX = 400;
+    public static final double ORDINARY_EXPENSES_DISCOUNT = 0.8;
+    public static final double SPECIAL_EXPENSES_DISCOUNT = 0.5;
 
     public double calculate(double originalPrice, double cumulativeExpensesThisMonth) {
-        if (cumulativeExpensesThisMonth >= 100 && cumulativeExpensesThisMonth < 150) {
-            return originalPrice * 0.8;
+        if (cumulativeExpensesThisMonth >= ORDINARY_EXPENSES_MIN && cumulativeExpensesThisMonth < ORDINARY_EXPENSES_MAX) {
+            return originalPrice * ORDINARY_EXPENSES_DISCOUNT;
         }
-        if (cumulativeExpensesThisMonth >= 150 && cumulativeExpensesThisMonth < 400) {
-            return originalPrice * 0.5;
+        if (cumulativeExpensesThisMonth >= ORDINARY_EXPENSES_MAX && cumulativeExpensesThisMonth < SPECIAL_EXPENSES_MAX) {
+            return originalPrice * SPECIAL_EXPENSES_DISCOUNT;
         }
         return originalPrice;
     }
