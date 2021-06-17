8dfa5758ce05f3eef295ce8be5c06ae22c677c39
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-17 12:52:10 +0800

    [Felicity] refactor: rename variable in MorseCodeDecoder

--- src/main/java/com/tw/academy/basic/$4_naming/practiceOne/MorseCodeDecoder.java
+++ src/main/java/com/tw/academy/basic/$4_naming/practiceOne/MorseCodeDecoder.java
@@ -2,15 +2,15 @@ package com.tw.academy.basic.$4_naming.practiceOne;
 
 public class MorseCodeDecoder {
     public static String decode(String input) {
-        String[] tempValue = input.split("\\s{2,}");
-        StringBuilder res = new StringBuilder();
-        for (String s : tempValue) {
-            String [] charArray = s.split(" ");
-            for (String c: charArray) {
-                res.append(MorseCode.get(c));
+        String[] words = input.split("\\s{2,}");
+        StringBuilder decodedMorseCode = new StringBuilder();
+        for (String word : words) {
+            String [] singleLetters = word.split(" ");
+            for (String singleLetter: singleLetters) {
+                decodedMorseCode.append(MorseCode.get(singleLetter));
             }
-            res.append(" ");
+            decodedMorseCode.append(" ");
         }
-        return res.toString().replaceAll("null", "").trim();
+        return decodedMorseCode.toString().replaceAll("null", "").trim();
     }
 }
