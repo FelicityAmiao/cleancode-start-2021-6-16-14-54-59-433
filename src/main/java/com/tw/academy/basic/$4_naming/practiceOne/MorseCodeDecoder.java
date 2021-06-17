56d9f83e3241ef306076c2ff0d48e1f0f3562da3
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-17 12:58:09 +0800

    [Felicity] refactor: eliminate magic number

--- src/main/java/com/tw/academy/basic/$4_naming/practiceOne/MorseCodeDecoder.java
+++ src/main/java/com/tw/academy/basic/$4_naming/practiceOne/MorseCodeDecoder.java
@@ -1,16 +1,22 @@
 package com.tw.academy.basic.$4_naming.practiceOne;
 
 public class MorseCodeDecoder {
+
+    public static final String TWO_AND_MORE_SPACE_PATTERN = "\\s{2,}";
+    public static final String WORD_SEPARATOR = " ";
+    public static final String LETTER_SEPARATOR = " ";
+    public static final String NON_MORSE_CODE_CHAR = "null";
+
     public static String decode(String input) {
-        String[] words = input.split("\\s{2,}");
+        String[] words = input.split(TWO_AND_MORE_SPACE_PATTERN);
         StringBuilder decodedMorseCode = new StringBuilder();
         for (String word : words) {
-            String [] singleLetters = word.split(" ");
+            String [] singleLetters = word.split(LETTER_SEPARATOR);
             for (String singleLetter: singleLetters) {
                 decodedMorseCode.append(MorseCode.get(singleLetter));
             }
-            decodedMorseCode.append(" ");
+            decodedMorseCode.append(WORD_SEPARATOR);
         }
-        return decodedMorseCode.toString().replaceAll("null", "").trim();
+        return decodedMorseCode.toString().replaceAll(NON_MORSE_CODE_CHAR, "").trim();
     }
 }
